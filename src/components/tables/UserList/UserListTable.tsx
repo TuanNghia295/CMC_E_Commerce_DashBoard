8b0c4679dc6bb2c69a61be6819fd61c7bfade3a4
TuanNghia <tuannghiait2905@gmail.com>
2026-01-19 17:54:48 +0700

    Update

--- src/components/tables/UserList/UserListTable.tsx
+++ src/components/tables/UserList/UserListTable.tsx
@@ -337,7 +337,7 @@ export default function UserListTable() {
                             ) : (
                               <div className="h-8 w-8 rounded-full bg-gray-200 dark:bg-gray-700 flex items-center justify-center">
                                 <span className="text-xs text-gray-500 dark:text-gray-400">
-                                  {user.full_name.charAt(0).toUpperCase()}
+                                  {user.full_name?.charAt(0).toUpperCase()}
                                 </span>
                               </div>
                             )}
