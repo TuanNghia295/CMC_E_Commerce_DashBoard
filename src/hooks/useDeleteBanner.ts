eb6a57a842b83808fbd32af5d59ea87897cf5cf2
TuanNghia <tuannghiait2905@gmail.com>
2026-01-21 23:35:04 +0700

    Update banner

--- src/hooks/useDeleteBanner.ts
+++ src/hooks/useDeleteBanner.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { bannerApi } from "../services/bannerService";
 import { toast } from "react-hot-toast";
